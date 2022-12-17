efffbb14b273597413790003d9a47f4f75dfc53f
Nathan <salomb3@gmail.com>
2022-12-17 13:57:27 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char bookName[70];
+
+    printf("\n\n\t\t*** WELCOME TO LIBRARY APPLICATION ***\n\n");
+    printf("\t\t\nENTER THE NAME OF THE BOOK YOU WOULD LIKE TO LOAN OR RETURN: \n");
+
+    gets(bookName);
+
+    printf("\nThis book is unavailable: %s " , bookName);
+    printf("\n\n");
+    return 0;
+
+}
